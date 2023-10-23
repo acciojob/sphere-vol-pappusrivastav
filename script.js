ef3ec6e784c93b2826c7771c133cbdadde88cf2e
pappusrivastav <144468141+pappusrivastav@users.noreply.github.com>
2023-10-23 14:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function volume_sphere() {
     //Write your code here
+	var p,radius,;
+	p=3.14;
+	var radius=document.getElementById('MyForm');
+	var volume=p*(4/3)*radius*radius*radius;
+	return volume;
   
 } 
 
